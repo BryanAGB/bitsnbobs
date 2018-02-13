e835d0ff68a001c35de1ae3654f2def5a8123475
BryanAGB <bryan@bryanmansell.co.uk>
2018-02-13 09:05:57 +0000

    Create alertPopUp.swift

---
+++ alertPopUp.swift
@@ -0,0 +1,3 @@
+let alert = UIAlertController(title: "Warning", message: "You have unsaved changes.", preferredStyle: UIAlertControllerStyle.alert)
+            alert.addAction(UIAlertAction(title: "Ok", style: UIAlertActionStyle.default, handler: nil))
+            self.present(alert, animated: true, completion: nil)
